e5f46a4e50dc34a80576b09b2821be39c00a06aa
TonyGamer <tomblackaz@gmail.com>
2024-05-31 10:22:21 -0700

    Removed Config Trasnfer Code

--- Config/LoadstoneConfig.cs
+++ Config/LoadstoneConfig.cs
@@ -93,47 +93,5 @@ public static class LoadstoneConfig
 				new ConfigDescription(
 					"The volume of the loading music",
 					acceptableValues: new AcceptableValueRange<float>(0, 1.5f)));
-
-
-		TryRemoveOldEntries();
-	}
-
-	private static void TryRemoveOldEntries()
-	{
-		var OldPostLoadStartDelay = LoadstoneFile.Bind<float>(
-				"Tweaks.Lethal.PostLoadStartDelay",
-				"Post-load Start Delay",
-				PostLoadStartDelay.Value,
-				new ConfigDescription(""));
-		
-		var OldPostGenerateSpawnDelay = LoadstoneFile.Bind<float>(
-				"Tweaks.Lethal.PostGenerateSpawnDelay",
-				"Post-generate Spawn Delay",
-				PostGenerateSpawnDelay.Value,
-				new ConfigDescription(""));
-
-		var OldShouldGenAsync = LoadstoneFile.Bind<bool>(
-				"Tweaks.Dungeon.ShouldGenAsync",
-				"Should Dungeon Generate Asynchronously",
-				ShouldGenAsync.Value,
-				new ConfigDescription(""));
-
-		var OldDungeonAsyncMaxTime = LoadstoneFile.Bind<float>(
-				"Tweaks.Dungeon.DungeonAsyncMaxTime",
-				"Async Dungeon Generation Max Time per Frame",
-				DungeonAsyncMaxTime.Value,
-				new ConfigDescription(""));
-
-		PostLoadStartDelay.Value = OldPostLoadStartDelay.Value;
-		PostGenerateSpawnDelay.Value = OldPostGenerateSpawnDelay.Value;
-		ShouldGenAsync.Value = OldShouldGenAsync.Value;
-		DungeonAsyncMaxTime.Value = OldDungeonAsyncMaxTime.Value == 100f ? 30f : OldDungeonAsyncMaxTime.Value;
-
-		LoadstoneFile.Remove(OldPostLoadStartDelay.Definition);
-		LoadstoneFile.Remove(OldPostGenerateSpawnDelay.Definition);
-		LoadstoneFile.Remove(OldShouldGenAsync.Definition);
-		LoadstoneFile.Remove(OldDungeonAsyncMaxTime.Definition);
-
-		LoadstoneFile.Save();
 	}
 }
