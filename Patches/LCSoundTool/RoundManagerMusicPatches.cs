23b6aadb25ba32f7b356014135d4759a56798397
TonyGamer <tomblackaz@gmail.com>
2024-06-13 09:12:37 -0700

    Fixed music audio clip name

--- Patches/LCSoundTool/RoundManagerMusicPatches.cs
+++ Patches/LCSoundTool/RoundManagerMusicPatches.cs
@@ -10,7 +10,7 @@ namespace Loadstone.Patches.LCSoundTool;
 public class RoundManagerMusicPatches
 {
 	static internal AudioSource loadingAudioSource;
-	static internal AudioClip loadingAudioClip = SoundTool.GetAudioClip("AdiBTW-Loadstone", "LoadstoneLoading.ogg");
+	static internal AudioClip loadingAudioClip = SoundTool.GetAudioClip($"AdiBTW-{PluginInfo.PLUGIN_NAME}", "LoadstoneLoading.ogg");
 
 	[HarmonyPatch("Awake")]
 	[HarmonyPrefix]
